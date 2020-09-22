61cc39eaf6e1bc8d3a8a969b39091b002d7ff7d4
Daniel Chapman <scriptor.deo@gmail.com>
2020-09-21 20:21:35 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name;
+</script>
+
+<main class="text-center p-4 max-w-xs mx-auto my-0 sm-max-w-none">
+  <h1 class="text-red-600 uppercase text-6xl font-hairline">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
